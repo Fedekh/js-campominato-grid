0dbf9d05453e6667ae385a076be618bea5e53322
Fedekh <federicocet@gmail.com>
2023-03-17 14:37:16 +0100

    add first function declaration e see if works

--- js/script.js
+++ js/script.js
@@ -19,3 +19,45 @@
 // Di cosa ho bisogno per generare i numeri?
 // Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
 // Le validazioni e i controlli possiamo farli anche in un secondo momento.
+
+
+// DICHIARAZIONI
+// definisco in primis alcune variabili che mi serviranno con scope globale 
+const btn = document.querySelector(".btn"); // questo è il buttone che mi indicherà poi la difficoltà
+const grid = document.querySelector(".grid") // griglia le cui caselle dipenderanno dalal difficoltà selezionata
+let numberSquare = 100; //numero di caselle, di default 100, ma cambiando difficoltà cambieranno
+
+// FUNZIONI
+
+/**
+ * Description mi genera la griglia con gli elementi, dipendentemente dal numero di elementi in argomento
+ * @param {text} numberOfGrid 
+ * @returns {array}
+ */
+function generateGrid (numberOfGrid){
+    for (let k = 1 ; k <= numberOfGrid; k++){
+        let currentElem = document.createElement("div"); //creo un elemento nel dom
+        currentElem.classList.add("grid-elem"); //aggiungo la classe stilizzata in css all'elemento appena creato
+        currentElem.innerText = k; // imposto il test (o numero) che dovrà poi comparire nella casella nel DOM
+        grid.appendChild(currentElem); //aggiungo l'elemento alal griglia
+        // console.log(currentElem, typeof currentElem);
+    }
+    return currentElem;
+}
+
+generateGrid(numberSquare);
+
+
+/**
+ * Description mi genera 1 singolo elemento (square) della griglia
+ * @param {any} 
+ * @returns {any}
+ */
+function generateGridElem () {
+    const square = document.createElement("div"); //mi crea un elemento con tag div
+    square.classList.add("grid-elem"); //aggiungo la classe grid-elem configurata con css
+    return square;
+
+}
+
+
